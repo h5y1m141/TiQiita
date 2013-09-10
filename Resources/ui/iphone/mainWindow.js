9ad2b9654a1b57566ccffc787480f809537d7743
hiroshi oyamada <h5y1m141@gmail.com>
2013-09-10 19:39:24 +0900

    アプリ起動時のメイン画面を準備

--- Resources/ui/iphone/mainWindow.js
+++ Resources/ui/iphone/mainWindow.js
@@ -1,10 +1,12 @@
-var mainWindow;
+var mainWindow,
+  __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
 mainWindow = (function() {
 
   function mainWindow() {
-    var t,
-      _this = this;
+    this.refresData = __bind(this.refresData, this);
+
+    var file, json, myTemplate, t, testData;
     this.baseColor = {
       barColor: "#f9f9f9",
       backgroundColor: "#f9f9f9",
@@ -19,23 +21,124 @@ mainWindow = (function() {
     });
     this._createNavbarElement();
     t = Titanium.UI.create2DMatrix().scale(0);
-    this.table = Ti.UI.createTableView({
-      backgroundColor: "#f3f3f3",
-      separatorStyle: Titanium.UI.iPhone.TableViewSeparatorStyle.NONE,
-      width: 160,
-      height: 'auto',
-      left: 150,
-      top: 20,
-      borderColor: "#f3f3f3",
-      borderWidth: 2,
-      borderRadius: 10,
-      zIndex: 10,
-      transform: t
+    myTemplate = {
+      childTemplates: [
+        {
+          type: "Ti.UI.ImageView",
+          bindId: "icon",
+          properties: {
+            defaultImage: "ui/image/logo.png",
+            width: 40,
+            height: 40,
+            left: 5,
+            top: 5
+          }
+        }, {
+          type: "Ti.UI.Label",
+          bindId: "title",
+          properties: {
+            color: "#333",
+            font: {
+              fontSize: 16,
+              fontFamily: 'Rounded M+ 1p'
+            },
+            width: 240,
+            height: 20,
+            left: 60,
+            top: 25
+          }
+        }, {
+          type: "Ti.UI.Label",
+          bindId: "handleName",
+          properties: {
+            color: "#333",
+            font: {
+              fontSize: 12,
+              fontFamily: 'Rounded M+ 1p'
+            },
+            width: 200,
+            height: 15,
+            left: 60,
+            top: 5
+          }
+        }, {
+          type: "Ti.UI.Label",
+          bindId: "updateTime",
+          properties: {
+            color: "#333",
+            font: {
+              fontSize: 12,
+              fontFamily: 'Rounded M+ 1p'
+            },
+            width: 60,
+            height: 15,
+            right: 0,
+            top: 5
+          }
+        }, {
+          type: "Ti.UI.Label",
+          bindId: "contents",
+          properties: {
+            color: "#333",
+            font: {
+              fontSize: 12,
+              fontFamily: 'Rounded M+ 1p'
+            },
+            width: 240,
+            height: 50,
+            left: 60,
+            top: 45
+          }
+        }
+      ]
+    };
+    this.listView = Ti.UI.createListView({
+      templates: {
+        template: myTemplate
+      },
+      defaultItemTemplate: "template"
     });
-    this.table.addEventListener('click', function(e) {});
-    this.window.add(this.table);
+    testData = Titanium.Filesystem.getFile(Titanium.Filesystem.resourcesDirectory, "model/testData.json");
+    file = testData.read().toString();
+    json = JSON.parse(file);
+    this.refresData(json);
+    this.window.add(this.listView);
     return this.window;
   }
+
+  mainWindow.prototype.refresData = function(data) {
+    var dataSet, layout, section, sections, _i, _items, _len;
+    sections = [];
+    section = Ti.UI.createListSection();
+    dataSet = [];
+    for (_i = 0, _len = data.length; _i < _len; _i++) {
+      _items = data[_i];
+      layout = {
+        properties: {
+          height: 100
+        },
+        title: {
+          text: _items.title
+        },
+        icon: {
+          image: _items.user.profile_image_url
+        },
+        updateTime: {
+          text: _items.updated_at_in_words
+        },
+        handleName: {
+          text: _items.user.url_name
+        },
+        contents: {
+          text: _items.body.replace(/<\/?[^>]+>/gi, "")
+        }
+      };
+      dataSet.push(layout);
+    }
+    section.setItems(dataSet);
+    sections.push(section);
+    return this.listView.setSections(sections);
+  };
 
   mainWindow.prototype._createNavbarElement = function() {
     var windowTitle;
--- coffee/ui/iphone/mainWindow.coffee
+++ coffee/ui/iphone/mainWindow.coffee
@@ -14,25 +14,109 @@ class mainWindow
       
     @_createNavbarElement()
     t = Titanium.UI.create2DMatrix().scale(0)
-    
-    @table = Ti.UI.createTableView
-      backgroundColor:"#f3f3f3"
-      separatorStyle: Titanium.UI.iPhone.TableViewSeparatorStyle.NONE
-      width:160
-      height:'auto'
-      left:150
-      top:20
-      borderColor:"#f3f3f3"
-      borderWidth:2
-      borderRadius:10
-      zIndex:10
-      transform:t
+    myTemplate = childTemplates:[
+      # icon
+      type: "Ti.UI.ImageView"
+      bindId:"icon"
+      properties:
+        defaultImage:"ui/image/logo.png"
+        width:40
+        height:40
+        left:5
+        top:5
+    ,
+      # title
+      type: "Ti.UI.Label"
+      bindId:"title"
+      properties:
+        color: "#333"
+        font:
+          fontSize:16
+          fontFamily : 'Rounded M+ 1p'
+        width:240
+        height:20
+        left:60
+        top:25
+    ,
+      # handleName
+      type: "Ti.UI.Label"
+      bindId:"handleName"
+      properties:
+        color: "#333"
+        font:
+          fontSize:12
+          fontFamily : 'Rounded M+ 1p'
+        width:200
+        height:15
+        left:60
+        top:5
+    ,
+      # updateTime
+      type: "Ti.UI.Label"
+      bindId:"updateTime"
+      properties:
+        color: "#333"
+        font:
+          fontSize:12
+          fontFamily : 'Rounded M+ 1p'
+        width:60
+        height:15
+        right:0
+        top:5
+    ,
+      # contents
+      type: "Ti.UI.Label"
+      bindId:"contents"
+      properties:
+        color: "#333"
+        font:
+          fontSize:12
+          fontFamily : 'Rounded M+ 1p'
+        width:240
+        height:50
+        left:60
+        top:45
+        
+    ]      
+    @listView = Ti.UI.createListView
+      templates:
+        template: myTemplate
+      defaultItemTemplate: "template"
 
-    @table.addEventListener('click',(e) =>
-    )
-    
-    @window.add @table
+    testData = Titanium.Filesystem.getFile(Titanium.Filesystem.resourcesDirectory, "model/testData.json")
+    file = testData.read().toString()
+    json = JSON.parse(file)
+
+    @refresData(json)
+    @window.add @listView
     return @window
+  refresData: (data) =>        
+
+    sections = []
+    section = Ti.UI.createListSection()
+      
+    dataSet = []
+    # 都道府県のエリア毎に都道府県のrowを生成
+    for _items in data
+      layout =
+        properties:
+          height:100
+        title:
+          text: _items.title
+        icon:
+          image: _items.user.profile_image_url
+        updateTime:
+          text: _items.updated_at_in_words
+        handleName:
+          text: _items.user.url_name
+        contents:
+          text: _items.body.replace(/<\/?[^>]+>/gi, "")
+      dataSet.push(layout)
+
+    section.setItems dataSet
+    sections.push section
+
+    return @listView.setSections sections
     
   _createNavbarElement:() ->
     windowTitle = Ti.UI.createLabel
@@ -46,5 +130,6 @@ class mainWindow
 
     @window.setTitleControl windowTitle
     return
+    
     
 module.exports = mainWindow  
