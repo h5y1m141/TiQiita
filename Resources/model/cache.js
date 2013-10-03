b34ee25a38c1acb93c9dce42193d9dbdc57bbbf8
hiroshi oyamada <h5y1m141@gmail.com>
2013-10-04 07:31:17 +0900

    キャッシュに登録済かどうか判定する際にRecordsetがnullかどうかで判定していたが存在しないレコードの場合にはnullではなく、Recordsetのlengthが0になるため判定処理を修正

--- Resources/model/cache.js
+++ Resources/model/cache.js
@@ -8,24 +8,42 @@ Cache = (function() {
     db.file.setRemoteBackup(false);
     db.execute("CREATE TABLE IF NOT EXISTS pagination(category TEXT, lastURL TEXT, loadedPageURL TEXT, nextURL TEXT);");
     db.close();
+    db = null;
     return;
   }
 
   Cache.prototype.setPageState = function(category, nextURL, lastURL, loadedPageURL) {
-    var db, sql;
+    var checkData, db, rs, sql;
     db = Ti.Database.open('tiqiita');
-    sql = "INSERT INTO pagination VALUES ('" + category + "', '" + lastURL + "', '" + loadedPageURL + "', '" + nextURL + "');";
+    checkData = "SELECT category FROM pagination where category = '" + category + "';";
+    rs = db.execute(checkData);
+    if (rs.rowCount === 0) {
+      sql = "INSERT INTO pagination VALUES ('" + category + "', '" + lastURL + "', '" + loadedPageURL + "', '" + nextURL + "');";
+    } else {
+      Ti.API.info("update table");
+      sql = "UPDATE pagination SET category = '" + category + "' , lastURL ='" + lastURL + "', loadedPageURL = '" + loadedPageURL + "',nextURL = '" + nextURL + "' where category = '" + category + "';";
+      Ti.API.info(sql);
+    }
     db.execute(sql);
     db.close();
+    db = null;
   };
 
   Cache.prototype.showPageState = function(category) {
-    var db, rs, sql;
+    var db, obj, rs, sql;
     db = Ti.Database.open('tiqiita');
     sql = "SELECT category,nextURL,lastURL,loadedPageURL FROM pagination where category = '" + category + "';";
     rs = db.execute(sql);
     Ti.API.info(rs);
-    return rs.fieldByName("category");
+    obj = {
+      category: rs.fieldByName("category"),
+      nextURL: rs.fieldByName("nextURL"),
+      loadedPageURL: rs.fieldByName("loadedPageURL"),
+      lastURL: rs.fieldByName("lastURL")
+    };
+    db.close();
+    db = null;
+    return obj;
   };
 
   return Cache;
--- coffee/model/cache.coffee
+++ coffee/model/cache.coffee
@@ -10,13 +10,25 @@ class Cache
     # ページ遷移状態を管理するためのTableを生成
     db.execute "CREATE TABLE IF NOT EXISTS pagination(category TEXT, lastURL TEXT, loadedPageURL TEXT, nextURL TEXT);"
     db.close()
+    db = null
     return
     
   setPageState:(category,nextURL,lastURL,loadedPageURL) ->
     db = Ti.Database.open('tiqiita')
-    sql = "INSERT INTO pagination VALUES ('#{category}', '#{lastURL}', '#{loadedPageURL}', '#{nextURL}');"
+    checkData = "SELECT category FROM pagination where category = '#{category}';"
+    rs = db.execute checkData
+    # Ti.API.info "recordSet is #{rs.rowCount}"
+    if rs.rowCount is 0
+       
+      sql = "INSERT INTO pagination VALUES ('#{category}', '#{lastURL}', '#{loadedPageURL}', '#{nextURL}');"
+    else
+      Ti.API.info "update table"
+      sql = "UPDATE pagination SET category = '#{category}' , lastURL ='#{lastURL}', loadedPageURL = '#{loadedPageURL}',nextURL = '#{nextURL}' where category = '#{category}';"
+      Ti.API.info sql
+      
     db.execute sql
     db.close()
+    db = null
     
     return
     
@@ -27,7 +39,15 @@ class Cache
 
     rs = db.execute sql
     Ti.API.info rs
-    return rs.fieldByName("category")
+    obj =
+      category: rs.fieldByName("category")
+      nextURL:rs.fieldByName("nextURL")
+      loadedPageURL:rs.fieldByName("loadedPageURL")
+      lastURL:rs.fieldByName("lastURL")
+      
+    db.close()
+    db = null
+    return obj
     
         
 module.exports = Cache  
